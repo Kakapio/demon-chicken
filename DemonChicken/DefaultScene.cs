3daef29f021e3845e475cec9c8c8a2f2e798389c
Roshan Patel <ovicior@gmail.com>
2020-10-03 00:26:26 -0400

    init commit

---
+++ DemonChicken/DefaultScene.cs
@@ -0,0 +1,28 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nez;
+using Nez.Sprites;
+
+namespace DemonChicken
+{
+	public class DefaultScene : Scene
+    {
+        public override void Initialize()
+        {
+            SetDesignResolution(Screen.Width, Screen.Height, Scene.SceneResolutionPolicy.None);
+
+#if debug
+            CreateEntity("demo imgui draw commands")
+                .SetPosition(new Vector2(150, 150))
+                .AddComponent<DemoComponent>()
+                .AddComponent(new PrototypeSpriteRenderer(20, 20));
+#endif 
+
+            var logo = Content.LoadTexture(@"Content\Textures\nez-logo-black.png");
+            CreateEntity("logo")
+                .SetPosition(Screen.Center)
+                .AddComponent(new SpriteRenderer(logo));
+        }
+    }
+}
+
